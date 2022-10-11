960bb53f89a0e5d3d0d51691c8ee6c5ab18c30d3
dcen7 <mdcengiz@gmail.com>
2022-10-11 06:49:17 -0500

    first commit

---
+++ ListsOfChecks/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  ListsOfChecks
+//
+//  Created by Mehmet Deniz Cengiz on 7/11/22.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text  = ""
+    var checked = false
+}
