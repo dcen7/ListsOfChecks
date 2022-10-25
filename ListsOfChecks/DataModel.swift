633e2ecb01c9ac0eb420ab29a0ce4da95ce4a152
dcen7 <mdcengiz@gmail.com>
2022-10-25 12:40:14 -0500

    create data model and initialize it in scene delegate

---
+++ ListsOfChecks/DataModel.swift
@@ -0,0 +1,53 @@
+//
+//  DataModel.swift
+//  ListsOfChecks
+//
+//  Created by Mehmet Deniz Cengiz on 10/21/22.
+//
+
+import Foundation
+
+class DataModel {
+    var lists = [Checklist]()
+    
+    init() {
+        loadChecklists()
+    }
+    
+    //MARK: - Saving data and loading data
+    func saveChecklist() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(lists)
+            
+            try data.write(to: dataFilePath(),
+                           options: Data.WritingOptions.atomic)
+        } catch {
+            print("Error encoding item array: \(error.localizedDescription)")
+        }
+    }
+
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+
+    func dataFilePath() -> URL {
+        documentsDirectory().appendingPathComponent("Checklist.plist")
+    }
+
+    func loadChecklists() {
+        let path = dataFilePath()
+        
+        if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+                lists = try decoder.decode([Checklist].self, from: data)
+            } catch {
+                print("Error decoding item array: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
